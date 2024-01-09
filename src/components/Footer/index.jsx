f99a67a084d7ff9dd7539b380b5363b82ebba3d1
tiagolimar <tiagolima.r2@gmail.com>
2024-01-09 13:03:03 -0300

    styling footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,17 @@
+import "./style.css"
+
 export const Footer = () => {
     return (
-        <footer className="mt-5 mb-4 bg-black">
-            <div className="container d-flex gap-4">
-                <div className="main">
-                    <div className="section-main">DIGITAL STORES</div>
+        <footer>
+            <div className="container d-flex gap-4 p-4">
+                <div className="section-top">
+                    <div className="section-main">DIGITAL STORE</div>
                     <div className="section-media">REDES SOCIAIS</div>
                 </div>
-                <div className="section-information list-group">
+                <div className="section-information">
                     <h3>Informação</h3>
                     <ul>
-                        <li className="list-decoration-none">Sobre Drip Store</li>
+                        <li>Sobre Drip Store</li>
                         <li>Segurança</li>
                         <li>Wishlist</li>
                         <li>Blog</li>
@@ -30,9 +32,8 @@ export const Footer = () => {
                 <section className="contact">
                     <h3>Contato</h3>
                     <address>
-                        {" "}
                         Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza -
-                        CE, 60150-161{" "}
+                        CE, 60150-161
                     </address>
                     <address> (85) 3051-3411 </address>
                 </section>
