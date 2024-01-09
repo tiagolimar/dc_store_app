cc60301dccdb1dd67a67c7b317986ad6c433ac07
tiagolimar <tiagolima.r2@gmail.com>
2024-01-09 19:18:58 -0300

    src imagem logo navbar

--- src/components/_Geral/Logo.jsx
+++ src/components/_Geral/Logo.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import "./Logo.css"
+import digital_store from "../../../assets/digital-store.svg"
 
 export const Logo = ()=>{
     return(
         <div className="d-flex gap-2">
-            <img src="../../../assets/digital-store.svg" alt="logo digital store" className="ms-4"/>
+            <img src={digital_store} alt="logo digital store" className="ms-4"/>
             <Link className="navbar-brand logo-digital" to="/">Digital Store</Link>
         </div>
     )
