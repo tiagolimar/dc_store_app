5d86e73a6ab78588d9563254c6ae9a35cbcf9099
tiagolimar <tiagolima.r2@gmail.com>
2024-01-11 20:41:45 -0300

    building carousel

--- src/pages/Home/components/Carrosel.jsx
+++ src/pages/Home/components/Carrosel.jsx
@@ -1,45 +1,60 @@
-import tenis from "../../../../assets/tenis.png"
+import tenis from "../../../../assets/tenis.png";
+// import circulo from "../../../assets/circulo.svg";
+import "./style.css";
 
-const ButtonLeft = ()=>{
-    return(
+const ButtonSlide = (props) => {
+    const active = props.active ? "active" : "";
+    const current = props.active ? "true" : "";
+    return (
         <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExample"
-        data-bs-slide="prev"
-    >
-        <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-        ></span>
-        <span className="visually-hidden">Previous</span>
-    </button>
-    )
-}
+            type="button"
+            data-bs-target="#carousel-slide"
+            data-bs-slide-to={props.num}
+            className={`btn-slide ${active}`}
+            aria-current={`${current}`}
+            aria-label={props.label}
+        ></button>
+    );
+};
 
-const ButtonRight = ()=>{
-    return(
-        <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExample"
-        data-bs-slide="next"
-    >
-        <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-        ></span>
-        <span className="visually-hidden">Next</span>
-    </button>
-    )
-}
+const CarosselItem = (props) => {
+    const active = props.active ? "active" : "";
+    return (
+        <div className={`carousel-item ${active}`}>
+            <div className="slide-left ">
+                <h3 className="slide-left-title">
+                    Melhores ofertas personalizadas
+                </h3>
+                <h2 className="slide-left-subtile">Queima de stoque Nike 🔥</h2>
+                <p className="slide-left-text">
+                    Consequat culpa exercitation mollit nisi excepteur do do
+                    tempor laboris eiusmod irure consectetur.
+                </p>
+                <button className="slide-left-btn">Ver Ofertas</button>
+            </div>
+            <div className="slide-right w-45">
+                <img src={tenis} className="d-block w-10" alt="..." />
+            </div>
+        </div>
+    );
+};
 
 export const Carrosel = () => {
     return (
-        <div id="carouselExample" className="carousel slide">
+        <div id="carousel-slide" className="carousel slide">
+            <div className="carousel-indicators">
+                <ButtonSlide active="true" num={0} label="Slide 1" />
+                <ButtonSlide num={1} label="Slide 2" />
+                <ButtonSlide num={2} label="Slide 3" />
+                <ButtonSlide num={3} label="Slide 4" />
+            </div>
             <div className="carousel-inner">
-                <div className="carousel-item active">
+                <CarosselItem active="true" />
+                {/* <div className="carousel-item active">
                     <img src={tenis} className="d-block w-30" alt="..." />
+                </div> */}
+                <div className="carousel-item">
+                    <img src={tenis} className="d-block me-5w-30" alt="..." />
                 </div>
                 <div className="carousel-item">
                     <img src={tenis} className="d-block w-30" alt="..." />
@@ -48,8 +63,6 @@ export const Carrosel = () => {
                     <img src={tenis} className="d-block w-30" alt="..." />
                 </div>
             </div>
-            <ButtonLeft />
-            <ButtonRight />
         </div>
     );
 };
