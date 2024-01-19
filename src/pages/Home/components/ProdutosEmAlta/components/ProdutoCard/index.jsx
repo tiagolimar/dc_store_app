5e92b37a547e91cbcfe14b26347332e237b2c615
tiagolimar <tiagolima.r2@gmail.com>
2024-01-18 21:40:15 -0300

    ...

--- src/pages/Home/components/ProdutosEmAlta/components/ProdutoCard/index.jsx
+++ src/pages/Home/components/ProdutosEmAlta/components/ProdutoCard/index.jsx
@@ -4,21 +4,25 @@ import "./style.css"
 export const ProdutoCard = (props) => {
     const produto = props.data
     const desconto = produto.desconto ? `${produto.desconto}% OFF` : ""
+    const carregado = props.carregado ? "" : "unload"
+
     return (
-        <div className="produto-alta-card mb-3">
-            <div className="produto-alta-bg">
-                <div className={`produto-alta-bg-${desconto?"desconto":"sem-desconto"} ms-3 mt-3`}>{desconto}</div>
-                <img src={tenisAlta} alt="" />
-            </div>
-            <div className="produto-alta-text">
-                <div className="produto-alta-categoria">{produto.categoria}</div>
-                <div className="produto-alta-nome">{produto.nome}</div>
-                <div className="produto-alta-preco d-flex">
-                    <div className="produto-alta-preco-antigo">
-                        R${produto.precoAntes}
-                    </div>
-                    <div className="produto-alta-preco-atual ms-3">
-                        R${produto.precoDepois}
+        <div className={`produto-alta-card-placement ${carregado}`}>
+            <div className="produto-alta-card mb-3">
+                <div className="produto-alta-bg">
+                    <div className={`produto-alta-bg-${desconto?"desconto":"sem-desconto"} ms-3 mt-3`}>{desconto}</div>
+                    <img src={tenisAlta} alt="" />
+                </div>
+                <div className="produto-alta-text">
+                    <div className="produto-alta-categoria">{produto.categoria}</div>
+                    <div className="produto-alta-nome">{produto.nome}</div>
+                    <div className="produto-alta-preco d-flex">
+                        <div className="produto-alta-preco-antigo">
+                            R${produto.precoAntes}
+                        </div>
+                        <div className="produto-alta-preco-atual ms-3">
+                            R${produto.precoDepois}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Home/components/ProdutosEmAlta/index.jsx
+++ src/pages/Home/components/ProdutosEmAlta/index.jsx
@@ -2,28 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { ProdutoCard } from './components/ProdutoCard';
 import { ProdutoCardGroup } from './components/ProdutoCardGroup/';
+import produtosFake from "./components/produtosFake.js";
 
 export const ProdutosEmAlta = () => {
-    let [produtos, setProdutos] = useState([]);
+    let [produtos, setProdutos] = useState(produtosFake);
+    let [carregado, setCarregado] = useState(false)
 
     const getProdutos = async () => {
         const response = await axios.get(
             "https://dc-store-api-ka0t.onrender.com/api/produtos"
         );
         setProdutos(response.data);
+        
+        if (response.data) setCarregado(true);
     };
 
     useEffect(() => {
         getProdutos();
     }, []);
 
-    if (!produtos) return null;
-
     return (
         <ProdutoCardGroup>{
             produtos.map((produto,id)=>{
-                return <ProdutoCard data={produto} key={id} />
-            
+                return <ProdutoCard data={produto} key={id} carregado={carregado} />
             })
         }</ProdutoCardGroup>)
 };
