f89427bb55a2e3b91305f5b58831263be195f08e
tiagolimar <tiagolima.r2@gmail.com>
2024-01-20 04:43:00 -0300

    estilizacao produto em destaque

--- src/pages/Home/components/ProdutoEmDestaque/index.jsx
+++ src/pages/Home/components/ProdutoEmDestaque/index.jsx
@@ -1,19 +1,27 @@
-
-// import tenisJordan from "../../../../../assets/"
-import "./style.css"
+import tenisJordan from "../../../../../assets/tenis-jordan.png";
+import "./style.css";
 
 export const ProdutoEmDestaque = () => {
     return (
-    <section className="container d-flex produto-em-destaque mb-5 mt-5">
-        <div className="prod-em-dest-esquerda">
-            img
-        </div>
-        <div className="prod-em-dest-direta ms-5">
-            <p className="prod-em-dest-direita-titulo">Oferta especial</p>
-            <h3 className="prod-em-dest-direita-nome">Air Jordan edição de colecionador</h3>
-            <p className="prod-em-dest-direita-texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit saepe obcaecati eius non, maiores laborum dolorem repellat aspernatur mollitia autem ea dicta assumenda natus debitis eveniet incidunt fugiat tenetur quod?</p>
-            <button className="slide-left-btn">Ver Ofertas</button>
-        </div>
-    </section>
-    )
+        <section className="container d-flex produto-em-destaque mb-5 mt-5 justify-content-center">
+            <div className="prod-em-dest-esquerda">
+                <div className="prod-em-dest-circle">
+                    <img src={tenisJordan} alt="produto em destaque" />
+                </div>
+            </div>
+            <div className="prod-em-dest-direta ms-5">
+                <p className="prod-em-dest-direita-titulo">Oferta especial</p>
+                <h3 className="prod-em-dest-direita-nome">
+                    Air Jordan edição de colecionador
+                </h3>
+                <p className="prod-em-dest-direita-texto">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Suscipit saepe obcaecati eius non, maiores laborum dolorem
+                    repellat aspernatur mollitia autem ea dicta assumenda natus
+                    debitis eveniet incidunt fugiat tenetur quod?
+                </p>
+                <button className="slide-left-btn">Ver Ofertas</button>
+            </div>
+        </section>
+    );
 };
