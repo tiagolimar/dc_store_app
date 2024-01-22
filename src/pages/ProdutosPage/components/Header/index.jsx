47e2285396c8c4c526ed64fbc1040a176ab47194
tiagolimar <tiagolima.r2@gmail.com>
2024-01-22 07:49:13 -0300

    page produtos

---
+++ src/pages/ProdutosPage/components/Header/index.jsx
@@ -0,0 +1,16 @@
+export const Header = () => {
+    return (
+        <header className="d-flex justify-content-between align-items-center me-5 mb-4">
+            <div className="produtos-resultado d-flex gap-2">
+                <p className="produtos-resultado-titulo">{`Resultados para "{Tênis}" - `}</p>
+                <p className="produtos-resultado-total">{`{200} produtos`}</p>
+            </div>
+            <div className="produtos-ordernar border border-secondary rounded d-flex gap-2">
+                <p className="produtos-ordernar-titulo">Ordernar por:</p>
+                <select name="" id="">
+                    <option value="0">opcao 01</option>
+                </select>
+            </div>
+        </header>
+    );
+};
--- src/pages/ProdutosPage/index.jsx
+++ src/pages/ProdutosPage/index.jsx
@@ -1,23 +1,35 @@
-import {Produtos} from '../../components/Produtos';
-import { MenuFiltrar } from './components/MenuFiltrar';
-import "./style.css"
+import { useEffect, useState } from "react";
+import produtosFake from './../../components/Produtos/components/produtosFake';
+import { Produtos } from "../../components/Produtos";
+import { MenuFiltrar } from "./components/MenuFiltrar";
+import { Header } from "./components/Header";
+import "./style.css";
+import axios from "axios";
 
 export const ProdutosPage = () => {
+    let [produtos, setProdutos] = useState(produtosFake);
+    let [carregado, setCarregado] = useState(false);
+
+    const getProdutos = async () => {
+        const response = await axios.get(
+            "https://dc-store-api-ka0t.onrender.com/api/produtos"
+        );
+
+        setProdutos(response.data);
+
+        if (response.data) {
+            setCarregado(true);
+        }
+    };
+
+    useEffect(() => {
+        getProdutos();
+    }, []);
+
     return (
-        <section className='page-produto mt-4'>
-            <header className='d-flex justify-content-between align-items-center me-5 mb-4'>
-                <div className="produtos-resultado d-flex gap-2">
-                    <p className='produtos-resultado-titulo'>{`Resultados para "{Tênis}" - `}</p>
-                    <p className='produtos-resultado-total'>{`{200} produtos`}</p>
-                </div>
-                <div className="produtos-ordernar border border-secondary rounded d-flex gap-2">
-                    <p className='produtos-ordernar-titulo'>Ordernar por:</p>
-                    <select name="" id="">
-                        <option value="0">opcao 01</option>
-                    </select>
-                </div>
-            </header>
-            <main className='d-flex justify-content-center'>
+        <section className="page-produto mt-4">
+            <Header />
+            <main className="d-flex justify-content-center">
                 <MenuFiltrar />
                 <div className="produtos-container">
                     <Produtos noTitle={true} quantidade={50} />
